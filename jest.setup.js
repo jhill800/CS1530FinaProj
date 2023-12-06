490ea70a916d659966a464d9e8c8c2fe55d9a4fe
samherbst <samherbst73@gmail.com>
2023-12-05 19:45:50 -0500

    Create unit test

---
+++ jest.setup.js
@@ -0,0 +1,23 @@
+global.window = {
+    addEventListener: jest.fn(),
+};
+const localStorageMock = (function() {
+    let store = {};
+    return {
+        getItem(key) {
+            return store[key] || null;
+        },
+        setItem(key, value) {
+            store[key] = value.toString();
+        },
+        removeItem(key) {
+            delete store[key];
+        },
+        clear() {
+            store = {};
+        }
+    };
+})();
+
+global.localStorage = localStorageMock;
+global.window.localStorage = localStorageMock;

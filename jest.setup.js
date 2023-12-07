64419e3be0848877d5cd3854543d56bf67bc7b03
samherbst <samherbst73@gmail.com>
2023-12-06 21:30:29 -0500

    Passing unit test

--- jest.setup.js
+++ jest.setup.js
@@ -4,17 +4,26 @@ global.window = {
 const localStorageMock = (function() {
     let store = {};
     return {
-        getItem(key) {
-            return store[key] || null;
+        key(key) {
+            return key;
         },
         setItem(key, value) {
             store[key] = value.toString();
+            this.length = Object.keys(store).length;
         },
         removeItem(key) {
             delete store[key];
+            this.length = Object.keys(store).length;
         },
         clear() {
             store = {};
+            this.length = 0;
+        },
+        getItem(key){
+            return store[key];
+        },
+        get length() {
+            return Object.keys(store).length;
         }
     };
 })();
--- unitest.test.js
+++ unitest.test.js
@@ -4,18 +4,15 @@ const { retrieveItemsFromLocalStorage } = require('./script.js');
 describe('retrieveItemsFromLocalStorage', () => {
     beforeEach(() => {
         window.localStorage.clear();
-        window.localStorage.setItem('item1', 'value1');
-        window.localStorage.setItem('item2', 'value2');
+        window.localStorage.setItem(0, 'value1');
+        window.localStorage.setItem(1, 'value2');
     });
 
     it('should retrieve items from localStorage and return them as an array of objects', () => {
         expect(window.addEventListener).toHaveBeenCalledWith('load', expect.any(Function));
-        window.localStorage.clear();
-        window.localStorage.setItem('item1', 'value1');
-        window.localStorage.setItem('item2', 'value2');
         const expected = [
-            { key: 'item1', value: 'value1' },
-            { key: 'item2', value: 'value2' }
+            { key: 0, value: 'value1' },
+            { key: 1, value: 'value2' }
         ];
         const result = retrieveItemsFromLocalStorage();
         expect(result).toEqual(expected);
